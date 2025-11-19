29e23a5163097391e4f55fc19103e4c260ba5c14
Ferri Kusuma <cklothox79@gmail.com>
2025-11-20 01:47:05 +0700

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,26 +1,16 @@
 import pandas as pd
-from pathlib import Path
+import yaml
 
-DATA_DIR = Path(__file__).resolve().parent.parent / "data"
-RT_DIRS = {
-    "rt1": DATA_DIR / "rt1" / "data_rt1.csv",
-    "rt2": DATA_DIR / "rt2" / "data_rt2.csv",
-    "rt3": DATA_DIR / "rt3" / "data_rt3.csv",
-}
+def load_info(path="config/info_wilayah.yaml"):
+    with open(path, "r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
 
-def load_rt(rt_key: str) -> pd.DataFrame:
-    """
-    Memuat data dari CSV untuk RT yang ditentukan.
-    """
-    path = RT_DIRS.get(rt_key)
-    if not path.exists():
-        return pd.DataFrame(columns=["no_kk", "nama_kepala_keluarga", "alamat", "rt", "rw", "kode_pos", 
-                                     "desa_kelurahan", "kecamatan", "kabupaten_kota", "provinsi", "tanggal_dikeluarkan"])
-    return pd.read_csv(path)
 
-def save_rt(rt_key: str, df: pd.DataFrame):
-    """
-    Menyimpan data ke CSV untuk RT yang ditentukan.
-    """
-    path = RT_DIRS.get(rt_key)
-    df.to_csv(path, index=False)
+def load_warga(path="data/warga.csv"):
+    df = pd.read_csv(path, dtype=str)
+
+    # convert numerik
+    df["rt"] = df["rt"].astype(int)
+    df["rw"] = df["rw"].astype(int)
+
+    return df
